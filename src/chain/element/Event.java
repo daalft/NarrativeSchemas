e287c273c4664fa04f84bdb6890f1a5c5f5a7bb5
daalft <blueygh2@gmail.com>
2013-07-01 14:42:50 +0200

    Added comments.

--- src/chain/element/Event.java
+++ src/chain/element/Event.java
@@ -3,34 +3,68 @@ package chain.element;
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
-
+/**
+ * Class to represent an event as a typed dependency 
+ * consisting of a verb lemma, an ID and one or more mentions.
+ */
 public class Event implements Serializable, Comparable<Event> {
 
 	/**
-	 * 
+	 * Serial ID
 	 */
 	private static final long serialVersionUID = -7900717167065083054L;
+	/**
+	 * Information
+	 */
 	private String typedDep, mention, id;
+	/**
+	 * List of mentions
+	 */
 	private Set<String> mentions;
+	/**
+	 * Internal event score
+	 */
 	private int globalScore = 0;
+	/**
+	 * Maximum event score as scored against a chain
+	 */
 	private double maxScore = 0.0;
 	
+	/**
+	 * No-argument constructor
+	 */
 	public Event () {
 		mentions = new HashSet<String>();
 	}
 
+	/**
+	 * Sets the maximum score for this Event
+	 * @param d score
+	 */
 	public void setMaxScore (double d) {
 		maxScore = d;
 	}
 	
+	/**
+	 * Returns the maximum score for this Event
+	 * @return max score
+	 */
 	public double getMaxScore () {
 		return maxScore;
 	}
 	
+	/**
+	 * Increments this score by the given value <em>i</em>
+	 * @param i value
+	 */
 	public void incrementScore (int i) {
 		globalScore += i;
 	}
 	
+	/**
+	 * Constructor with full information
+	 * @param information full information
+	 */
 	public Event (String information) {
 		String[] sp = information.split(" ");
 		if (sp.length < 2)
@@ -42,25 +76,48 @@ public class Event implements Serializable, Comparable<Event> {
 		id = sp[4];
 	}
 
+	/**
+	 * Returns the mention of this Event at position <em>pos</em>
+	 * @param pos position
+	 * @return mention
+	 */
 	public String getMention (int pos) {
 		String[] s = mentions.toArray(new String[0]);
 		return s[pos];
 	}
 	
+	/**
+	 * Constructor with full, explicit information
+	 * @param d dependency
+	 * @param s mention
+	 * @param i id
+	 */
 	public Event (String d, String s, String i) {
 		typedDep = d;
 		mention = s;
 		id = i;
 	}
 
+	/**
+	 * Adds a mention to the list of mentions in this Event
+	 * @param mention mention
+	 */
 	public void addMention (String mention) {
 		mentions.add(mention);
 	}
 	
+	/**
+	 * Returns all mentions of this Event
+	 * @return list of mentions
+	 */
 	public Set<String> getMentions () {
 		return mentions;
 	}
 	
+	/**
+	 * Returns only the verb lemma of this Event's typed dependency
+	 * @return verb lemma
+	 */
 	public String getVerb () {
 		String[] sp = typedDep.split("-");
 		return sp[0];
@@ -87,30 +144,61 @@ public class Event implements Serializable, Comparable<Event> {
 		return d;
 	}
 
+	/**
+	 * Returns the typed dependency of this Event
+	 * <p>
+	 * Typed dependency consists of verb lemma and 
+	 * dependency (subject, object, preposition)
+	 * @return typed dependency
+	 */
 	public String getTypedDep() {
 		return typedDep;
 	}
 
+	/**
+	 * Sets the typed dependency of this Event
+	 * @param typedDep typed dependency
+	 */
 	public void setTypedDep(String typedDep) {
 		this.typedDep = typedDep;
 	}
 
+	/**
+	 * Returns the mention of this Event
+	 * @return mention
+	 */
 	public String getMention() {
 		return mention;
 	}
 
+	/**
+	 * Sets the mention of this Event
+	 * @param mention mention
+	 */
 	public void setMention(String mention) {
 		this.mention = mention;
 	}
-
+	
+	/**
+	 * Returns the ID of this Event
+	 * @return ID
+	 */
 	public String getId() {
 		return id;
 	}
 
+	/**
+	 * Sets the ID of this Event
+	 * @param id id
+	 */
 	public void setId(String id) {
 		this.id = id;
 	}
 
+	/**
+	 * toString method
+	 * @return string representation
+	 */
 	public String toString () {
 		StringBuilder sb = new StringBuilder();
 		sb.append(typedDep);
@@ -139,6 +227,9 @@ public class Event implements Serializable, Comparable<Event> {
 		return false;
 	}
 
+	/**
+	 * Comparison based on global score
+	 */
 	@Override
 	public int compareTo(Event e) {
 		if (e.globalScore < this.globalScore)
