146fa35b1d807a069aab19e03ea8ca9dfa0e7452
daalft <blueygh2@gmail.com>
2013-07-01 14:50:23 +0200

    Added comments.

--- src/chain/element/EventBlock.java
+++ src/chain/element/EventBlock.java
@@ -3,31 +3,55 @@ package chain.element;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+* Class used to represent a block of events
+*/
 public class EventBlock implements Serializable {
 
 	/**
-	 * 
+	 * Serial ID
 	 */
 	private static final long serialVersionUID = 4560149494312112667L;
+	/**
+	 * Events in this block
+	 */
 	private List<Event> events;
 	
+	/**
+	 * No-argument constructor
+	 */
 	public EventBlock () {
 		events = new ArrayList<Event>();
 	}
 	
+	/**
+	 * Constructor with EventBlock
+	 * @param e EventBlock
+	 */
 	public EventBlock (EventBlock e) {
 		events = e.getEvents();
 	}
 	
+	/**
+	 * Adds an Event to this EventBlock
+	 * @param e event to add
+	 */
 	public void add (Event e) {
 		events.add(e);
 	}
 	
+	/**
+	 * Returns all events in this block
+	 * @return list of Event
+	 */
 	public List<Event> getEvents () {
 		return events;
 	}
 
+	/**
+	 * toString method
+	 * @return string representation
+	 */
 	public String toString () {
 		StringBuilder sb = new StringBuilder();
 		for (Event e : events) {
